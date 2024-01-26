00771e19c2ee52b0dfe20f7448163a81c2013711
fedemoretto11 <fedemoretto94@gmail.com>
2024-01-26 16:12:48 -0300

    Inicio detalle ordenes

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,43 +1,47 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import Card from './Card'
-import { Feather } from '@expo/vector-icons';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import Card from "./Card";
+import { Feather } from "@expo/vector-icons";
 
-const OrderItem = ({ order }) => {
+const OrderItem = ({ order, setModalVisible, setOrderId }) => {
+  return (
+    <Card style={styles.cartItemContainer}>
+      <View>
+        <Text style={styles.createdAt}>
+          Creada el {new Date(order.createdAt).toLocaleString()}
+        </Text>
+        <Text style={styles.total}>Total: ${order.total}</Text>
+      </View>
+      <TouchableOpacity 
+        style={styles.searchIcon} 
+        onPress={() => {
+          setModalVisible(true)
+          setOrderId(order.orderId)
+        }}
+      >
+        <Feather name="search" size={24} color="black" />
+      </TouchableOpacity>
+    </Card>
+  );
+};
 
-
-    return (
-        <Card style={styles.cartItemContainer}>
-            <View >
-                <Text style={styles.createdAt}>
-                    Creada el {new Date(order.createdAt).toLocaleString()}
-                </Text>
-                <Text style={styles.total}>Total: ${order.total}</Text>
-            </View>
-            <TouchableOpacity style={styles.searchIcon} onPress={null}>
-                <Feather name="search" size={24} color="black" />
-            </TouchableOpacity>
-        </Card>
-    )
-}
-
-export default OrderItem
+export default OrderItem;
 
 const styles = StyleSheet.create({
-    cartItemContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        padding: 20,
-    },
-    searchIcon: {
-        marginLeft: 'auto',
-    },
-    createdAt:{
-        fontFamily: 'Raleway-Regular',
-        marginBottom:5,
-    },
-    total:{
-        fontFamily: 'Raleway-Bold',
-        fontSize:14,
-    }
-})
+  cartItemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 20,
+  },
+  searchIcon: {
+    marginLeft: "auto",
+  },
+  createdAt: {
+    fontFamily: "Raleway-Regular",
+    marginBottom: 5,
+  },
+  total: {
+    fontFamily: "Raleway-Bold",
+    fontSize: 14,
+  },
+});
--- src/screens/OrdersScreen.jsx
+++ src/screens/OrdersScreen.jsx
@@ -1,5 +1,5 @@
 import { 
-  FlatList 
+  FlatList, Modal, StyleSheet, Text, TouchableOpacity, View 
 } from 'react-native'
 
 import OrderItem from '../components/OrderItem'
@@ -7,6 +7,7 @@ import OrderItem from '../components/OrderItem'
 import { useGetOrdersQuery } from '../services/shopService'
 import { useSelector } from 'react-redux'
 import { useEffect, useState } from 'react'
+import { COLORS } from '../global/colors'
 
 const OrdersScreen = () => {
 
@@ -15,7 +16,10 @@ const OrdersScreen = () => {
   const { data, isLoading, error} = useGetOrdersQuery(localId)
   const [ orderData, setOrderData ] = useState([])
 
-  console.log(localId)
+  const [modalVisible, setModalVisible] = useState(false)
+  const [orderId, setOrderId] = useState("")
+  const [orderSelected, setOrderSelected] = useState({})
+
   
   useEffect(() => {
     if (data) {
@@ -24,17 +28,82 @@ const OrdersScreen = () => {
     }
   }, [data, isLoading])
 
+  useEffect(() => {
+    const orderSelected = orderData.find(order => order.orderId === orderId)
+    setOrderSelected(orderSelected)
+  }, [orderId])
+
   const renderOrderItem = ({ item }) => {
     return (
-      <OrderItem order={item} />
+      <OrderItem order={item} setModalVisible={setModalVisible} setOrderId={setOrderId} />
     )
   }
   
   return (
-    <FlatList
-      data={orderData}
-      renderItem={renderOrderItem}
-    />
+    <>
+      <FlatList
+        data={orderData}
+        renderItem={renderOrderItem}
+      />
+      <Modal visible={modalVisible} animationType='fade'>
+        <View style={styles.modal}>
+          <View style={styles.innerModal}>
+            <FlatList 
+              data={orderSelected?.cartItems}
+            />
+            <Text style={styles.modalText}>{orderSelected?.total}</Text>
+            <TouchableOpacity
+              style={styles.modalBtn}
+              onPress={() => {setModalVisible(false)}}
+            >
+              <Text style={styles.btnText}>Volver</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </>
   )
 }
 export default OrdersScreen
+
+const styles = StyleSheet.create({
+  modal: {
+    flex: 1,
+    marginTop: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: COLORS.primary,
+  },
+  innerModal: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 20,
+    textAlign: "center"
+  },
+  modalBtn: {
+    width: 100,
+    padding: 12,
+    borderRadius: 10,
+    elevation: 20,
+    backgroundColor: COLORS.third,
+  },
+  btnText: {
+    textAlign: "center",
+    fontFamily: "Raleway-Bold",
+    fontSize: 20,
+    color: COLORS.secondary
+  }
+})
